75230b837316e8d594393caa40c5e1aaf1b441cf
molokochris <molokochrisp742@gmail.com>
2024-04-30 21:40:50 +0200

    secret message

--- Math_Strings_Exceptions-Handling_more/secret_message.py
+++ Math_Strings_Exceptions-Handling_more/secret_message.py
@@ -5,7 +5,7 @@ for char in message:
     secret_message += str(ord(char))
 
 for i in range(0, len(secret_message)-1, 2):
-    decrypted_message += chr(int(secret_message[i]) + chr(int(secret_message[i+1])))
+    decrypted_message += chr(int(secret_message[i] + secret_message[i + 1]))
 
 print("secret message: ", secret_message)
 print("decrypted message: ", decrypted_message)
