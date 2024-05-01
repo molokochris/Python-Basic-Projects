f72d93d16eea10799c70046705465ea5fba1ea48
molokochris <molokochrisp742@gmail.com>
2024-05-01 16:43:25 +0200

    fix messages with spaces

--- Math_Strings_Exceptions-Handling_more/secret_message.py
+++ Math_Strings_Exceptions-Handling_more/secret_message.py
@@ -2,10 +2,17 @@ message = str(input("Please enter your message in uppercase: "))
 secret_message = ""
 decrypted_message = ""
 for char in message:
-    secret_message += str(ord(char) - 32)
+    if not(ord(char) == 32):
+        secret_message += str(ord(char) - 32)
+    else:
+        secret_message += str(ord(char))
 
 for i in range(0, len(secret_message)-1, 2):
-    decrypted_message += chr(int(secret_message[i] + secret_message[i + 1]) + 32)
+    if not(int(secret_message[i] + secret_message[i + 1]) == 32):
+        decrypted_message += chr(int(secret_message[i] + secret_message[i + 1]) + 32)
+    else:
+        decrypted_message += chr(int(secret_message[i] + secret_message[i + 1]))
+
 
 print("secret message: ", secret_message)
 print("decrypted message: ", decrypted_message)
