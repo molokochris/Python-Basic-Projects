36ca9f7d9319341baf6a7dbed671fcf14e5ae4c2
molokochris <molokochrisp742@gmail.com>
2024-05-01 16:25:36 +0200

    more

--- Math_Strings_Exceptions-Handling_more/secret_message.py
+++ Math_Strings_Exceptions-Handling_more/secret_message.py
@@ -2,10 +2,10 @@ message = str(input("Please enter your message in uppercase: "))
 secret_message = ""
 decrypted_message = ""
 for char in message:
-    secret_message += str(ord(char))
+    secret_message += str(ord(char) - 32)
 
 for i in range(0, len(secret_message)-1, 2):
-    decrypted_message += chr(int(secret_message[i] + secret_message[i + 1]))
+    decrypted_message += chr(int(secret_message[i] + secret_message[i + 1]) + 32)
 
 print("secret message: ", secret_message)
 print("decrypted message: ", decrypted_message)
