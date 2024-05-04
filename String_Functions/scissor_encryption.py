76c977e180323316e3f613bb1414641ea20a720e
molokochris <molokochrisp742@gmail.com>
2024-05-04 18:20:31 +0200

    alt solution

--- String_Functions/scissor_encryption.py
+++ String_Functions/scissor_encryption.py
@@ -1,6 +1,7 @@
 secret_message = input("Enter your message: ")
+num_shift = int(input("Enter number of characters to shift: "))
 encrypted_message = ""
-
+'''
 def to_binary(char):
     return ord(char)
 
@@ -23,3 +24,21 @@ for char in secret_message:
 
 print("secret message", secret_message)
 print("encrypted message", encrypted_message)
+'''
+# Even Better Solution:
+for char in secret_message:
+    to_binary = ord(char)
+    if (to_binary == 32):
+        encrypted_message += str(chr(to_binary))
+    elif (to_binary >= 120 and to_binary <= 122):
+        # encrypted_message += str(chr(to_binary - 23))
+        encrypted_message += str(chr(to_binary - (26 - num_shift)))
+    elif (to_binary >= 88 and to_binary <= 90):
+        # encrypted_message += str(chr(to_binary - 23))
+        encrypted_message += str(chr(to_binary - (26 - num_shift)))
+    else:
+        # encrypted_message += str(chr(to_binary + 3))
+        encrypted_message += str(chr(to_binary + num_shift))
+
+print("secret message: ", secret_message)
+print("encrypted message: ", encrypted_message)
