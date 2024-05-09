712b92167ac2b7cef8b36836b81b196387f547f3
molokochris <molokochrisp742@gmail.com>
2024-05-09 13:23:51 +0200

    warrior game

--- Object_Oriented_Programming/paul_sam_game.py
+++ Object_Oriented_Programming/paul_sam_game.py
@@ -10,23 +10,67 @@ Pauul has died and Sam is Victorious
 Game Over
 '''
 import random
-
-def randomNum():
-    return (random.randrange(100)/100) 
+import math
 
 class Warrior:
-    def __init__(self, name="", health=100, maxAttack=100, maxBlock=100):
+    def __init__(self, name="Warrior", health=0, maxAttack=0, maxBlock=0):
         self.name = name
         self.health = health
         self.maxAttack = maxAttack
         self.maxBlock = maxBlock
     
     def attack(self):
-        return randomNum() * self.maxAttack + .5
+        totalAttack = self.maxAttack * (random.random() + .5)
+        return totalAttack
     
     def block(self):
-        return randomNum() * self.maxBlock + .5
+        totalBlock = self.maxBlock * (random.random() + .5)
+
+        return totalBlock
 
 class Battle:
 
-# print(randomNum())
+    def startFight(self, warrior1, warrior2):
+        
+        while True:
+            if self.getAttactRes(warrior1, warrior2) == "Game Over":
+                print("Game Over")
+                break
+            
+            if self.getAttactRes(warrior2, warrior1) == "Game Over":
+                print("Game Over")
+                break
+
+    @staticmethod
+    def getAttactRes(warriorA, warriorB):
+
+        warriorAAttckAmt = warriorA.attack()
+
+        warriorBBlockAmt = warriorB.block()
+
+        warriorBDamage = math.ceil(warriorAAttckAmt - warriorBBlockAmt)
+
+        warriorB.health = warriorB.health - warriorBDamage
+
+        print("{} attacks {} and deals {} damage".format(warriorA.name, warriorB.name, warriorBDamage))
+
+        print("{} is down to {} health".format(warriorB.name, warriorB.health))
+
+        if warriorB.health <= 0:
+            print("{} has died and {} is victorious".format(warriorB.name, warriorA.name))
+
+            return "Game Over"
+        else:
+            return "Fight still on.."    
+
+def main():
+
+    Paul = Warrior("Paul", 50, 20, 10)
+    Sam = Warrior("Sam", 50, 20, 10)
+
+    battle = Battle()
+    battle.startFight(Sam, Paul)
+    # print()
+
+
+main()
