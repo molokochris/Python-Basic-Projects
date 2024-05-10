5c1b526705ef56f6f21c9a58ed0ac37c51855cb5
molokochris <molokochrisp742@gmail.com>
2024-05-10 09:06:13 +0200

    class inheritance, magic methods

---
+++ inheritance_polymorphism_and_magic_methods/magic_methods.py
@@ -0,0 +1,11 @@
+# __eq__ : Equal
+# __ne__ : Not Equal
+# __lt__ : Less than 
+# __gt__ : Greater than
+# __le__ : Less than or Equal
+# __ge__ : Greater than or Equal
+# __add__ : Addition
+# __sub__ : Subtraction
+# __mul__ : Multiplication
+# __div__ : Division
+# __mod__ : Modulus
