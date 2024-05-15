390b7bbd9bfff62073b58f357752c9e394631c1f
molokochris <molokochrisp742@gmail.com>
2024-05-15 11:09:13 +0200

    regular expressions & solved problem

--- regular_expressions/regular_expressions.py
+++ regular_expressions/regular_expressions.py
@@ -59,7 +59,7 @@ import re
 
 # randStr = "F.B.I. I.R.S. CIA"
 
-# print("Matches: ", len(re.findall(".\..\..\.", randStr)))
+# print("Matches: ", re.findall(".\..\..\.", randStr))
 
 # ------------------------------>
 
--- regular_expressions/solved_problem-0.py
+++ regular_expressions/solved_problem-0.py
@@ -4,9 +4,24 @@ import re
     -------------------- PROBLEM --------------------
     Create a regular expression that will match email addresses from
     a list
+    RULES:
     1. 1 to 20 lowercase and uppercase letters, numbers, plus ._%+-
     2. An @ symbol
     3. 2 to 20 lowercase and uppercase letters, numbers, plus .-
     4. A period
     5. 2 to 3 lowercase and uppercase letters
 '''
+
+emailList = ["molokochrisp742@gmail.com","mcpooedi@gmail.c1m", "w1ll8-po%+_@outlo-k..com"]
+
+for mail in emailList:
+    if (re.search("[\w._%+-]{1,20}@[\w.-]{2,20}\\.[A-Za-z]{2,3}", mail)):
+        print("{} - is a valid email".format(mail))
+    else:
+        print("{} - is not a valid email".format(mail))
+
+# SOLUTION 02:
+
+# counts the number of valid email addresses using findall(accepts strings as arguments)
+# after join the list items with a whitespace(turning list to string)
+print("Email matches:", len(re.findall("[\w._%+-]{1,20}@[\w.-]{2,20}\\.[A-Za-z]{2,3}", " ".join(emailList))))
